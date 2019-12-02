0d9c10f3bc5a68cc644fdecfe9efdecdc2370a4f
Janne Spijkervet <janne.spijkervet@gmail.com>
2019-12-02 18:03:23 +0100

    Code formatted with black

--- mirdata/billboard.py
+++ mirdata/billboard.py
@@ -14,35 +14,32 @@ import mirdata.utils as utils
 import mirdata.download_utils as download_utils
 import mirdata.jams_utils as jams_utils
 
-DATASET_DIR = 'McGill-Billboard'
+DATASET_DIR = "McGill-Billboard"
 INDEX_REMOTE = download_utils.RemoteFileMetadata(
-    filename='billboard-2.0-index.csv',
-    url='https://www.dropbox.com/s/o0olz0uwl9z9stb/billboard-2.0-index.csv?dl=1',
-    checksum='c47d304c212725998839cf9bb1a417aa',
+    filename="billboard-2.0-index.csv",
+    url="https://www.dropbox.com/s/o0olz0uwl9z9stb/billboard-2.0-index.csv?dl=1",
+    checksum="c47d304c212725998839cf9bb1a417aa",
     destination_dir=None,
 )
 
 ANNOTATIONS_REMOTE = download_utils.RemoteFileMetadata(
-    filename='billboard-2.0-salami_chords.tar.xz',
-    url='https://www.dropbox.com/s/p4xtixbvt4hw5c6/billboard-2.0-salami_chords.tar.xz?dl=1',
-    checksum='201b0c3c72cefe6c2fe697dc7886ce0d',
+    filename="billboard-2.0-salami_chords.tar.xz",
+    url="https://www.dropbox.com/s/p4xtixbvt4hw5c6/billboard-2.0-salami_chords.tar.xz?dl=1",
+    checksum="201b0c3c72cefe6c2fe697dc7886ce0d",
     destination_dir=None,
 )
 
 
 def _load_metadata(data_home):
 
-    metadata_path = os.path.join(
-        data_home,
-        'billboard-2.0-index.csv'
-    )
+    metadata_path = os.path.join(data_home, "billboard-2.0-index.csv")
 
     if not os.path.exists(metadata_path):
-        logging.info('Metadata file {} not found.'.format(metadata_path))
+        logging.info("Metadata file {} not found.".format(metadata_path))
         return None
 
-    with open(metadata_path, 'r') as fhandle:
-        reader = csv.reader(fhandle, delimiter=',')
+    with open(metadata_path, "r") as fhandle:
+        reader = csv.reader(fhandle, delimiter=",")
         next(reader, None)
         raw_data = []
         for line in reader:
@@ -53,21 +50,21 @@ def _load_metadata(data_home):
     for line in raw_data:
         track_id = line[0]
         metadata_index[track_id] = {
-            'chart_date': line[1],
-            'target_rank': int(line[2]) if line[2] else None,
-            'actual_rank': int(line[3]) if line[3] else None,
-            'title': line[4],
-            'artist': line[5],
-            'peak_rank': int(line[6]) if line[6] else None,
-            'weeks_on_chart': int(line[7]) if line[7] else None
+            "chart_date": line[1],
+            "target_rank": int(line[2]) if line[2] else None,
+            "actual_rank": int(line[3]) if line[3] else None,
+            "title": line[4],
+            "artist": line[5],
+            "peak_rank": int(line[6]) if line[6] else None,
+            "weeks_on_chart": int(line[7]) if line[7] else None,
         }
 
-    metadata_index['data_home'] = data_home
+    metadata_index["data_home"] = data_home
 
     return metadata_index
 
 
-DATA = utils.LargeData('billboard_index.json', _load_metadata)
+DATA = utils.LargeData("billboard_index.json", _load_metadata)
 
 
 class Track(object):
@@ -92,7 +89,7 @@ class Track(object):
 
     def __init__(self, track_id, data_home=None):
         if track_id not in DATA.index:
-            raise ValueError('{} is not a valid track ID in Salami'.format(track_id))
+            raise ValueError("{} is not a valid track ID in Salami".format(track_id))
 
         self.track_id = track_id
 
@@ -108,23 +105,23 @@ class Track(object):
         else:
             # annotations with missing metadata
             self._track_metadata = {
-                'chart_date': None,
-                'target_rank': None,
-                'actual_rank': None,
-                'title': None,
-                'artist': None,
-                'peak_rank': None,
-                'weeks_on_chart': None
+                "chart_date": None,
+                "target_rank": None,
+                "actual_rank": None,
+                "title": None,
+                "artist": None,
+                "peak_rank": None,
+                "weeks_on_chart": None,
             }
 
-        self.audio_path = os.path.join(self._data_home, self._track_paths['audio'][0])
-        self.chart_date = self._track_metadata['chart_date']
-        self.target_rank = self._track_metadata['target_rank']
-        self.actual_rank = self._track_metadata['actual_rank']
-        self.title = self._track_metadata['title']
-        self.artist = self._track_metadata['artist']
-        self.peak_rank = self._track_metadata['peak_rank']
-        self.weeks_on_chart = self._track_metadata['weeks_on_chart']
+        self.audio_path = os.path.join(self._data_home, self._track_paths["audio"][0])
+        self.chart_date = self._track_metadata["chart_date"]
+        self.target_rank = self._track_metadata["target_rank"]
+        self.actual_rank = self._track_metadata["actual_rank"]
+        self.title = self._track_metadata["title"]
+        self.artist = self._track_metadata["artist"]
+        self.peak_rank = self._track_metadata["peak_rank"]
+        self.weeks_on_chart = self._track_metadata["weeks_on_chart"]
 
     def __repr__(self):
         repr_string = (
@@ -134,23 +131,19 @@ class Track(object):
             + "SectionData('intervals', 'labels')"
         )
         return repr_string.format(
-            self.track_id,
-            self.audio_path,
-            self.chart_date,
-            self.title,
-            self.artist
+            self.track_id, self.audio_path, self.chart_date, self.title, self.artist
         )
 
     @utils.cached_property
     def chords(self):
         return _load_chords(
-            os.path.join(self._data_home, self._track_paths['salami'][0])
+            os.path.join(self._data_home, self._track_paths["salami"][0])
         )
 
     @utils.cached_property
     def sections(self):
         return _load_sections(
-            os.path.join(self._data_home, self._track_paths['salami'][0])
+            os.path.join(self._data_home, self._track_paths["salami"][0])
         )
 
     @property
@@ -172,19 +165,17 @@ def _load_chords(salami_path):
         chords_path (str):
 
     """
-    
+
     if salami_path is None or not os.path.exists(salami_path):
         return None
 
-    timed_chords = _timed_chords(
-            _parse_salami(salami_path)
-    )
+    timed_chords = _timed_chords(_parse_salami(salami_path))
 
     start_times, end_times, chords = [], [], []
     for tc in timed_chords:
-        start_times.append(tc['time'])
-        end_times.append(tc['time'] + tc['length'])
-        chords.append(tc['chord'])
+        start_times.append(tc["time"])
+        end_times.append(tc["time"] + tc["length"])
+        chords.append(tc["chord"])
 
     chord_data = utils.ChordData(np.array([start_times, end_times]).T, chords)
     return chord_data
@@ -200,15 +191,13 @@ def _load_sections(sections_path):
     if sections_path is None or not os.path.exists(sections_path):
         return None
 
-    timed_sections = _timed_sections(
-            _parse_salami(sections_path)
-    )
+    timed_sections = _timed_sections(_parse_salami(sections_path))
 
     start_times, end_times, sections = [], [], []
     for ts in timed_sections:
-        start_times.append(ts['time'])
-        end_times.append(ts['time'] + ts['length'])
-        sections.append(ts['section'])
+        start_times.append(ts["time"])
+        end_times.append(ts["time"] + ts["length"])
+        sections.append(ts["section"])
 
     section_data = utils.SectionData(np.array([start_times, end_times]).T, sections)
 
@@ -224,7 +213,7 @@ def _parse_salami(fn):
 
         Parse a salami_chords.txt file and return a dict with all the stuff innit
     """
-    s = open(fn).read().split('\n')
+    s = open(fn).read().split("\n")
     o = {}
     for x in s:
         if x.startswith("#"):
@@ -233,21 +222,21 @@ def _parse_salami(fn):
             if x[2:].startswith("artist:"):
                 o["artist"] = x[10:]
             if x[2:].startswith("metre:"):
-                o["meter"] = o.get("meter",[]) + [x[9:]]
+                o["meter"] = o.get("meter", []) + [x[9:]]
             if x[2:].startswith("tonic:"):
-                o["tonic"] = o.get("tonic",[]) + [x[9:]]
+                o["tonic"] = o.get("tonic", []) + [x[9:]]
         elif len(x) > 1:
-            spot = x.find('\t')
-            if spot>0:
+            spot = x.find("\t")
+            if spot > 0:
                 time = float(x[0:spot])
                 event = {}
                 event["time"] = time
-                rest = x[spot+1:]
-                items = rest.split(', ')
+                rest = x[spot + 1 :]
+                items = rest.split(", ")
                 for i in items:
                     chords = re.findall(r"(?=\| (.*?) \|)", i)
-                    section = i.split('|')
-                    if len(section) == 1 and not ('(' in section or ')' in section):
+                    section = i.split("|")
+                    if len(section) == 1 and not ("(" in section or ")" in section):
                         event["section"] = section[0]
                     if len(chords):
                         event["chords"] = chords
@@ -255,6 +244,7 @@ def _parse_salami(fn):
                         event["notes"] = event.get("notes", []) + [i]
                 o["events"] = o.get("events", []) + [event]
     return o
+
 
 def _timed_chords(parsed):
     """
@@ -267,28 +257,33 @@ def _timed_chords(parsed):
     """
     timed_chords = []
     tic = 0
-    for i,e in enumerate(parsed["events"]):
+    for i, e in enumerate(parsed["events"]):
         chords = []
         try:
-            dt = parsed["events"][i+1]["time"] - e["time"]
+            dt = parsed["events"][i + 1]["time"] - e["time"]
         except IndexError:
             dt = 0
 
         for c in e.get("chords", []):
-            for chord_string in c.split(" "): # TODO: figure the difference between | | and spaces
-                if chord_string == ".": 
+            for chord_string in c.split(
+                " "
+            ):  # TODO: figure the difference between | | and spaces
+                if chord_string == ".":
                     chords.append(chords[-1])
                 else:
                     # chords.append(Chord(chord_string))
                     chords.append(chord_string)
 
         tic = e["time"]
-        if (len(chords)):
+        if len(chords):
             seconds_per_chord = dt / float(len(chords))
             for c in chords:
-                timed_chords.append( {"time":tic, "chord":c, "length":seconds_per_chord} )
+                timed_chords.append(
+                    {"time": tic, "chord": c, "length": seconds_per_chord}
+                )
                 tic = tic + seconds_per_chord
     return timed_chords
+
 
 def _timed_sections(parsed):
     """
@@ -304,21 +299,23 @@ def _timed_sections(parsed):
     for i, e in enumerate(parsed["events"]):
         sections = []
         try:
-            dt = parsed["events"][i+1]["time"] - e["time"]
+            dt = parsed["events"][i + 1]["time"] - e["time"]
         except IndexError:
             dt = 0
-        
+
         section = None
-        if e.get('notes'):
-            if len(e.get('notes')) > 1:
-                section = (e.get('notes')[0], e.get('notes')[1])
+        if e.get("notes"):
+            if len(e.get("notes")) > 1:
+                section = (e.get("notes")[0], e.get("notes")[1])
             sections.append(section)
 
         tic = e["time"]
-        if (len(sections)):
+        if len(sections):
             seconds_per_chord = dt / float(len(sections))
             for c in sections:
-                timed_sections.append( {"time":tic, "section": c, "length": seconds_per_chord} )
+                timed_sections.append(
+                    {"time": tic, "section": c, "length": seconds_per_chord}
+                )
                 tic = tic + seconds_per_chord
     return timed_sections
 
@@ -357,7 +354,7 @@ def download(data_home=None, force_overwrite=False):
         force_overwrite=force_overwrite,
     )
 
-    annotations_dir = os.path.join(data_home, 'annotations')
+    annotations_dir = os.path.join(data_home, "annotations")
     if force_overwrite:
         if os.path.exists(annotations_dir):
             os.remove(annotations_dir)
@@ -369,7 +366,7 @@ def download(data_home=None, force_overwrite=False):
             else:
                 shutil.rmtree(os.path.join(data_home, DATASET_DIR))
 
-    
+
 def validate(data_home=None, silence=False):
     """Validate if the stored dataset is a valid version
 
